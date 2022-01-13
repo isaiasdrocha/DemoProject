7e15d77274cb8f077ebfd34bb7a397dee2dedf29
Isaias D Rocha Jr <isaiasdrocha@gmail.com>
2022-01-13 16:14:02 -0300

    Add more pageabe condictions

--- src/main/java/br/com/isaiasdrocha/demo/ProjectApplication.java
+++ src/main/java/br/com/isaiasdrocha/demo/ProjectApplication.java
@@ -2,9 +2,10 @@ package br.com.isaiasdrocha.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableSpringDataWebSupport
 public class ProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/isaiasdrocha/demo/forum/controller/TopicosController.java
+++ src/main/java/br/com/isaiasdrocha/demo/forum/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.isaiasdrocha.demo.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -11,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,10 +43,29 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd){
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao){
+		// v1/topicos?page=0&size=10&sort=id,ASC&sort=DataCriacao,desc
+		
+		if (nomeCurso == null) {
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			
+			return TopicoDto.converter(topicos);
+		}
+		else {
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+			return TopicoDto.converter(topicos);
+		}
+	}
+	
+	@GetMapping("/old")
+	public Page<TopicoDto> listaOld(@RequestParam(required = false) String nomeCurso, 
+			@RequestParam int pagina, 
+			@RequestParam int qtd,
+			@RequestParam String ordenacao){
 		
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			
