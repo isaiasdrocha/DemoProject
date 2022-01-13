accd537a8f33281e3cb75ed6593bbc61e98ed540
Isaias D Rocha Jr <isaiasdrocha@gmail.com>
2022-01-13 14:49:45 -0300

    404 Response implementation - not found

--- src/main/java/br/com/isaiasdrocha/demo/forum/controller/TopicosController.java
+++ src/main/java/br/com/isaiasdrocha/demo/forum/controller/TopicosController.java
@@ -2,12 +2,14 @@ package br.com.isaiasdrocha.demo.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,21 +62,42 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
-	public DetalhesTopicoDto detalhar(@PathVariable Long id) {
+	public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
 		
-		Topico topico = topicoRepository.getById(id);
-		return new DetalhesTopicoDto(topico);
+		Optional<Topico> topico = topicoRepository.findById(id);
+		
+		if (topico.isPresent())
+			return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+		else
+			return ResponseEntity.notFound().build();		
 	}
 	
 	//@PatchMapping //pequena alteraçao
 	@PutMapping("/{id}") //Alteracao completa - Mais comum no desenv
 	@Transactional // Avisa o spring que tem que realizar o commit no final do método
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		Optional<Topico> optional = topicoRepository.findById(id);
 		
-		Topico topico = form.atualizar(id, topicoRepository);
+		if (optional.isPresent()) {
+			
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		else
+			return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> remover(@PathVariable Long id){
+		Optional<Topico> optional = topicoRepository.findById(id);
 		
-		return ResponseEntity.ok(new TopicoDto(topico));
-		
+		if (optional.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} else
+			return ResponseEntity.notFound().build();		
+
 	}
 	
 }
